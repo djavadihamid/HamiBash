aa58b5673d1dc4affc285af796bf1ddc3cef5e9b
black <black.allballo@yandex.com>
2020-06-19 07:40:04 -0700

    [REFACTOR] [DOT_NET_CORE] delete old file

--- HamiDNCAliases.sh
+++
@@ -1,9 +0,0 @@
-createController(){
-        if [ -z "$1" ]
-                then
-                        echo "Name bd dada";
-                        return;
-        fi;
-
-        dotnet-aspnet-codeGenerator controller -name "$1"
-}
