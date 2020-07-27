9f82314bf439ae774d9e20cf9f2e023aa67c404a
djavadihamid <djavadi.hamid@yandex.com>
2020-07-27 13:26:41 -0700

    [FEAT][GIT] adding a function to delete submodule

--- HamiGitAliases.sh
+++ HamiGitAliases.sh
@@ -87,3 +87,20 @@ upSuMo(){
 InSuMo(){
         git submodule init
 }
+
+DeSuMo(){
+	if [ -z "$1" ]
+        then
+                echo "Provide path to submodule plaease.";
+                return;
+        fi;
+
+	# Remove the submodule entry from .git/config
+	git submodule deinit -f $1
+
+	# Remove the submodule directory from the superproject's .git/modules directory
+	rm -rf .git/modules/$1
+
+	# Remove the entry in .gitmodules and remove the submodule directory located at path/to/submodule
+	git rm -f $1
+}
