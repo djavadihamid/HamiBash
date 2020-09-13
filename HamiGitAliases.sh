d9e228cd3d7942452d121217c827b9bbd8edae28
unknown <Hamid@DESKTOP-J48AE5V.GarageGames.local>
2020-09-13 00:57:04 -0700

    [FEAT][GIT] pull all remote branches added

--- HamiGitAliases.sh
+++ HamiGitAliases.sh
@@ -128,3 +128,10 @@ AdSuMo(){
     InSuMo
     UpSuMo
 }
+
+
+PullAllBranches(){
+        for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
+                git branch --track ${branch#remotes/origin/} $branch
+        done
+}
